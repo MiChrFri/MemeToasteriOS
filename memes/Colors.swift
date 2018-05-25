64da0fef46ff09823c0e2199fe82510f54308044
Michael Frick <michaelfrick@thoughtworks.com>
2018-05-25 14:30:24 +0200

    adding app icons

--- memes/Colors.swift
+++ memes/Colors.swift
@@ -10,6 +10,7 @@ import UIKit
 
 struct Colors {
     static let navBarColor = UIColor.init(red: 255/255, green: 245/255, blue: 230/255, alpha: 1)
-    static let buttonText = UIColor.init(red: 176/255, green: 106/255, blue: 58/255, alpha: 1)
+    static let buttonText = UIColor.init(red: 78/255, green: 60/255, blue: 42/255, alpha: 1)
     static let background = UIColor.init(red: 242/255, green: 205/255, blue: 168/255, alpha: 1)
+    static let contrast = UIColor.init(red: 205/255, green: 162/255, blue: 118/255, alpha: 1)
 }
